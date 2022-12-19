8471bb9d3ccdba30d6666e9cc561508c0907b7eb
Nourm <105018259+Nourm9@users.noreply.github.com>
2022-12-19 01:11:06 -0500

    add:index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+class Deposit extends Transaction {
+  commit() {
+    this.account.balance += this.amount;
+  }
+}
+
+class Withdrawal extends Transaction {
+  commit() {
+    this.account.balance -= this.amount;
+  }
+};
 
 
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+}
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
